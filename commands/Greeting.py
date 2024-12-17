80d6a4d9686c4c157ede53659296bfdfed0d7446
송재욱 <haensol976520@gmail.com>
2024-12-17 16:47:50 +0900

    Fix: method 이름 중복

--- commands/Greeting.py
+++ commands/Greeting.py
@@ -9,33 +9,30 @@ class Greeting(commands.Cog):
     @commands.command(name="안녕", aliases=['아녕', '안녕하세요', '아녕하세요', '안ㄴ녕'], description="안녕")
     async def greet(self, ctx):
         if ctx.author.name in ["aiden300.", "aiden300_"]:
-            title = "야 이주언 싸려"
+            title = "🖕 야 이주언 싸려"
         elif random.random() <= 0.01:
-            title = "야 싸려" 
+            title = "🖕 야 싸려"
         else:
-            title = "안녕!"
+            title = "👋 안녕!"
             
         embed = discord.Embed(
             title=title,
-            color=discord.Color.red() if title != "안녕!" else discord.Color.purple()
+            color=discord.Color.red() if title != "👋 안녕!" else discord.Color.purple()
         )
         await ctx.reply(embed=embed)
         
     @commands.command(name="이주언", aliases=['주언'], description="이주언")
-    async def greet(self, ctx):
-        title = "병신"
+    async def greet_juwon(self, ctx):
         embed = discord.Embed(
-            title=title,
+            title="병신",
             color=discord.Color.purple()
         )
         await ctx.reply(embed=embed)
         
     @commands.command(name="앙", aliases=['한남'], description="앙")
-    async def greet(self, ctx):
-        title = "승일"
+    async def greet_seungil(self, ctx):
         embed = discord.Embed(
-            title=title,
+            title="🥵 승일",
             color=discord.Color.pink()
         )
         await ctx.reply(embed=embed)
-    
--- commands/Information.py
+++ commands/Information.py
@@ -7,9 +7,10 @@ class Information(commands.Cog):
 
     @commands.command(name="정보", aliases=['도움'], description="정보")
     async def information(self, ctx):
+        latency = round(self.bot.latency * 1000) 
         embed = discord.Embed(
-            title="JEE6",
-            description = "- [명령어](https://github.com/8G4B/JEE6.v1/blob/master/README.md)\n- [소스코드](https://github.com/8G4B/JEE6.v1)\n - [만든놈](https://github.com/976520)",
+            title="❗ JEE6",
+            description = f"- [명령어](https://github.com/8G4B/JEE6.v1/blob/master/README.md)\n- [소스코드](https://github.com/8G4B/JEE6.v1)\n- [만든놈](https://github.com/976520)\n\n- 핑: {latency}ms",
             color=discord.Color.yellow()
         )
         await ctx.reply(embed=embed)
