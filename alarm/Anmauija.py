e17c07c1778b2369b2f88cac4f456b2260e23d92
송재욱 <haensol976520@gmail.com>
2024-12-18 23:32:48 +0900

    Alarm

--- alarm/Anmauija.py
+++ alarm/Anmauija.py
@@ -10,15 +10,16 @@ class Anmauija(commands.Cog):
     def cog_unload(self):
         self.daily_alarm.cancel()
 
-    @tasks.loop(time=time(hour=20, minute=19))  
+    @tasks.loop(seconds=1)  
     async def daily_alarm(self):
-        for guild in self.bot.guilds:
-            for channel in guild.text_channels:
-                try:
-                    await channel.send("@everyone 안마의자 신청 1분 전\n [DOTORI 바로가기](https://www.dotori-gsm.com/home)")
-                    break 
-                except discord.Forbidden:
-                    continue  
+        if datetime.now().hour == 20 and datetime.now().minute == 19: # 20, 19
+            for guild in self.bot.guilds:
+                for channel in guild.text_channels:
+                    try:
+                        await channel.send("안마의자 신청 1분 전\n [DOTORI 바로가기](https://www.dotori-gsm.com/home)")
+                        break 
+                    except discord.Forbidden:
+                        continue  
 
     @daily_alarm.before_loop
     async def before_daily_alarm(self):
