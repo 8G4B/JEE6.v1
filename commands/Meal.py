57f79c60db6eed78c9e59191de6eb4684cd159d9
송재욱 <haensol976520@gmail.com>
2024-12-19 17:27:46 +0900

    refector: 책임에 따라 class를 분리

--- commands/Meal.py
+++ commands/Meal.py
@@ -50,181 +50,168 @@ class RequestMeal:
         except (requests.RequestException, json.JSONDecodeError, KeyError):
             return None
 
-class Meal(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class MealEmbed:
+    @staticmethod
+    def create_meal_embed(title: str, menu: str) -> discord.Embed:
+        return discord.Embed(
+            title=title,
+            description=menu.replace("<br/>", "\n"),
+            color=discord.Color.orange()
+        )
     
-    @commands.command(name='급식', description='급식 조회')
-    async def meal(self, ctx):
-        now = datetime.now()
+    @staticmethod
+    def create_error_embed(description: str) -> discord.Embed:
+        return discord.Embed(
+            title="❗ 오류",
+            description=description, 
+            color=discord.Color.red()
+        )
+
+class MealService:
+    def __init__(self):
+        self.meal_request = RequestMeal()
+    
+    def get_current_meal(self, now: datetime) -> tuple[str, str]:
         today = now.strftime("%Y%m%d")
         current_hour = now.hour
         current_minute = now.minute
         
         meal_info = RequestMeal.get_meal_info(today)
         
-        if meal_info:
-            if current_hour < 7 or (current_hour == 7 and current_minute < 30):
-                menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "1"), "NO_MEAL")
-                title = "🍳 아침"
-            elif current_hour < 12 or (current_hour == 12 and current_minute < 30):
-                menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "2"), "NO_MEAL")
-                title = "🍚 점심"
-            elif current_hour < 18 or (current_hour == 18 and current_minute < 30):
-                menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "3"), "NO_MEAL")
-                title = "🍖 저녁"
-            else:
-                tomorrow = (now + timedelta(days=1)).strftime("%Y%m%d")
-                tomorrow_meal_info = RequestMeal.get_meal_info(tomorrow)
-                if tomorrow_meal_info:
-                    menu = next((meal["DDISH_NM"] for meal in tomorrow_meal_info if meal["MMEAL_SC_CODE"] == "1"), "NO_MEAL")
-                    title = "🍳 내일 아침"
-                else:
-                    menu = "NO_MEAL"
-                    title = "🍳 내일 아침"
+        if not meal_info:
+            return None, None
             
-            embed = discord.Embed(
-                title=title,
-                description=menu.replace("<br/>", "\n"),
-                color=discord.Color.orange()
-            )
+        if current_hour < 7 or (current_hour == 7 and current_minute < 30):
+            menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "1"), NO_MEAL)
+            title = "🍳 아침"
+        elif current_hour < 12 or (current_hour == 12 and current_minute < 30):
+            menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "2"), NO_MEAL)
+            title = "🍚 점심"
+        elif current_hour < 18 or (current_hour == 18 and current_minute < 30):
+            menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "3"), NO_MEAL)
+            title = "🍖 저녁"
         else:
-            embed = discord.Embed(
-                title="❗ 오류",
-                description="나이스 API 이슈",
-                color=discord.Color.red()
-            )
+            tomorrow = (now + timedelta(days=1)).strftime("%Y%m%d")
+            tomorrow_meal_info = RequestMeal.get_meal_info(tomorrow)
+            if tomorrow_meal_info:
+                menu = next((meal["DDISH_NM"] for meal in tomorrow_meal_info if meal["MMEAL_SC_CODE"] == "1"), NO_MEAL)
+                title = "🍳 내일 아침"
+            else:
+                menu = NO_MEAL
+                title = "🍳 내일 아침"
+                
+        return title, menu
+        
+    def get_meal_by_type(self, date: str, meal_code: str, title: str) -> tuple[str, str]:
+        meal_info = RequestMeal.get_meal_info(date)
+        if not meal_info:
+            return None, None
+            
+        menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == meal_code), NO_MEAL)
+        return title, menu
+
+class Meal(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.meal_service = MealService()
+    
+    @commands.command(name='급식', description='급식 조회')
+    async def meal(self, ctx):
+        title, menu = self.meal_service.get_current_meal(datetime.now())
+        
+        if title and menu:
+            embed = MealEmbed.create_meal_embed(title, menu)
+        else:
+            embed = MealEmbed.create_error_embed("나이스 API 이슈")
             
         await ctx.reply(embed=embed)
         
     @commands.command(name='급식.아침', aliases=['급식.조식'], description='아침 조회')
     async def breakfast(self, ctx):
-        today = datetime.now().strftime("%Y%m%d")
-        meal_info = RequestMeal.get_meal_info(today)
+        title, menu = self.meal_service.get_meal_by_type(
+            datetime.now().strftime("%Y%m%d"),
+            "1",
+            "🍳 아침"
+        )
         
-        if meal_info:
-            breakfast_menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "1"), "NO_MEAL")
-            
-            embed = discord.Embed(
-                title="🍳 아침",
-                description=breakfast_menu.replace("<br/>", "\n"),
-                color=discord.Color.orange()
-            )
+        if title and menu:
+            embed = MealEmbed.create_meal_embed(title, menu)
         else:
-            embed = discord.Embed(
-                title="❗ 오류",
-                description="조식 정보를 가져올 수 없습니다.",
-                color=discord.Color.red()
-            )
+            embed = MealEmbed.create_error_embed("조식 정보를 가져올 수 없습니다.")
             
         await ctx.reply(embed=embed)
         
     @commands.command(name='급식.점심', aliases=['급식.중식'], description='점심 조회')
     async def lunch(self, ctx):
-        today = datetime.now().strftime("%Y%m%d")
-        meal_info = RequestMeal.get_meal_info(today)
+        title, menu = self.meal_service.get_meal_by_type(
+            datetime.now().strftime("%Y%m%d"),
+            "2", 
+            "🍚 점심"
+        )
         
-        if meal_info:
-            lunch_menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "2"), "NO_MEAL")
-            
-            embed = discord.Embed(
-                title="🍚 점심",
-                description=lunch_menu.replace("<br/>", "\n"),
-                color=discord.Color.orange()
-            )
+        if title and menu:
+            embed = MealEmbed.create_meal_embed(title, menu)
         else:
-            embed = discord.Embed(
-                title="❗ 오류",
-                description="중식 정보를 가져올 수 없습니다.",
-                color=discord.Color.red()
-            )
+            embed = MealEmbed.create_error_embed("중식 정보를 가져올 수 없습니다.")
         
         await ctx.reply(embed=embed)
         
     @commands.command(name='급식.저녁', aliases=['급식.석식'], description='저녁 조회')
     async def dinner(self, ctx):
-        today = datetime.now().strftime("%Y%m%d")
-        meal_info = RequestMeal.get_meal_info(today)
+        title, menu = self.meal_service.get_meal_by_type(
+            datetime.now().strftime("%Y%m%d"),
+            "3",
+            "🍖 저녁"
+        )
         
-        if meal_info:
-            dinner_menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "3"), "NO_MEAL")
-            
-            embed = discord.Embed(
-                title="🍖 저녁",
-                description=dinner_menu.replace("<br/>", "\n"),
-                color=discord.Color.orange()
-            )
+        if title and menu:
+            embed = MealEmbed.create_meal_embed(title, menu)
         else:
-            embed = discord.Embed(
-                title="❗ 오류",
-                description="석식 정보를 가져올 수 없습니다.",
-                color=discord.Color.red()
-            )
+            embed = MealEmbed.create_error_embed("석식 정보를 가져올 수 없습니다.")
             
         await ctx.reply(embed=embed)
         
     @commands.command(name='급식.내일아침', aliases=['급식.내일조식'], description='내일 아침 조회')
     async def tomorrow_breakfast(self, ctx):
-        tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y%m%d")
-        meal_info = RequestMeal.get_meal_info(tomorrow)
+        title, menu = self.meal_service.get_meal_by_type(
+            (datetime.now() + timedelta(days=1)).strftime("%Y%m%d"),
+            "1",
+            "🍳 내일 아침"
+        )
         
-        if meal_info:
-            breakfast_menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "1"), "NO_MEAL")
-            
-            embed = discord.Embed(
-                title="🍳 내일 아침",
-                description=breakfast_menu.replace("<br/>", "\n"),
-                color=discord.Color.orange()
-            )
+        if title and menu:
+            embed = MealEmbed.create_meal_embed(title, menu)
         else:
-            embed = discord.Embed(
-                title="❗ 오류",
-                description="내일 조식 정보를 가져올 수 없습니다.",
-                color=discord.Color.red()
-            )
+            embed = MealEmbed.create_error_embed("내일 조식 정보를 가져올 수 없습니다.")
             
         await ctx.reply(embed=embed)
     
     @commands.command(name='급식.내일점심', aliases=['급식.내일중식'], description='내일 점심 조회')
     async def tomorrow_lunch(self, ctx):
-        tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y%m%d")
-        meal_info = RequestMeal.get_meal_info(tomorrow)
+        title, menu = self.meal_service.get_meal_by_type(
+            (datetime.now() + timedelta(days=1)).strftime("%Y%m%d"),
+            "2",
+            "🍚 내일 점심"
+        )
         
-        if meal_info:
-            lunch_menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "2"), "NO_MEAL")
-            
-            embed = discord.Embed(
-                title="🍚 내일 점심",
-                description=lunch_menu.replace("<br/>", "\n"),
-                color=discord.Color.orange()
-            )
+        if title and menu:
+            embed = MealEmbed.create_meal_embed(title, menu)
         else:
-            embed = discord.Embed(
-                title="❗ 오류",
-                description="내일 중식 정보를 가져올 수 없습니다.",
-                color=discord.Color.red()
-            )
+            embed = MealEmbed.create_error_embed("내일 중식 정보를 가져올 수 없습니다.")
             
         await ctx.reply(embed=embed)
 
     @commands.command(name='급식.내일저녁', aliases=['급식.내일석식'], description='내일 저녁 조회')
     async def tomorrow_dinner(self, ctx):
-        tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y%m%d")
-        meal_info = RequestMeal.get_meal_info(tomorrow)
+        title, menu = self.meal_service.get_meal_by_type(
+            (datetime.now() + timedelta(days=1)).strftime("%Y%m%d"),
+            "3",
+            "🍖 내일 저녁"
+        )
         
-        if meal_info:
-            dinner_menu = next((meal["DDISH_NM"] for meal in meal_info if meal["MMEAL_SC_CODE"] == "3"), "NO_MEAL")
-            
-            embed = discord.Embed(
-                title="🍖 내일 저녁",
-                description=dinner_menu.replace("<br/>", "\n"),
-                color=discord.Color.orange()
-            )
+        if title and menu:
+            embed = MealEmbed.create_meal_embed(title, menu)
         else:
-            embed = discord.Embed(
-                title="❗ 오류",
-                description="내일 석식 정보를 가져올 수 없습니다.",
-                color=discord.Color.red()
-            )
+            embed = MealEmbed.create_error_embed("내일 석식 정보를 가져올 수 없습니다.")
             
         await ctx.reply(embed=embed)
