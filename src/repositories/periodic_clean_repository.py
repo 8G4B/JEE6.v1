cb49a6d95e22ae1806fb3c28f471ced2a83d228d
재욱 <haensol976520@gmail.com>
2025-05-19 14:12:02 +0900

    self.db

--- src/repositories/periodic_clean_repository.py
+++ src/repositories/periodic_clean_repository.py
@@ -4,17 +4,17 @@ from src.domain.models.periodic_clean import PeriodicClean
 from src.repositories.base_repository import BaseRepository
 
 class PeriodicCleanRepository(BaseRepository[PeriodicClean]):
-    def get_by_guild_and_channel(self, db: Session, guild_id: int, channel_id: int) -> Optional[PeriodicClean]:
-        return db.query(self.model).filter(
+    def get_by_guild_and_channel(self, guild_id: int, channel_id: int) -> Optional[PeriodicClean]:
+        return self.db.query(self.model).filter(
             self.model.guild_id == guild_id,
             self.model.channel_id == channel_id
         ).first()
 
-    def get_all_enabled(self, db: Session) -> List[PeriodicClean]:
-        return db.query(self.model).filter(self.model.enabled == True).all()
+    def get_all_enabled(self) -> List[PeriodicClean]:
+        return self.db.query(self.model).filter(self.model.enabled == True).all()
 
-    def enable(self, db: Session, guild_id: int, channel_id: int, interval_seconds: int):
-        record = self.get_by_guild_and_channel(db, guild_id, channel_id)
+    def enable(self, guild_id: int, channel_id: int, interval_seconds: int):
+        record = self.get_by_guild_and_channel(guild_id, channel_id)
         if record:
             record.enabled = True
             record.interval_seconds = interval_seconds
@@ -25,13 +25,13 @@ class PeriodicCleanRepository(BaseRepository[PeriodicClean]):
                 interval_seconds=interval_seconds,
                 enabled=True
             )
-            db.add(record)
-        db.commit()
+            self.db.add(record)
+        self.db.commit()
         return record
 
-    def disable(self, db: Session, guild_id: int, channel_id: int):
-        record = self.get_by_guild_and_channel(db, guild_id, channel_id)
+    def disable(self, guild_id: int, channel_id: int):
+        record = self.get_by_guild_and_channel(guild_id, channel_id)
         if record:
             record.enabled = False
-            db.commit()
+            self.db.commit()
         return record 
